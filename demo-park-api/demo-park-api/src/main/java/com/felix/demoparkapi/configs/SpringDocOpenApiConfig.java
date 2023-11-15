b26570994234f761a04cec6c92666224c1815184
Nathália Félix <55997404+nathaliafelix31@users.noreply.github.com>
2023-11-15 10:17:59 -0300

    Documentation SpringDoc OpenApi 3.1 and Swagger 3

--- demo-park-api/demo-park-api/src/main/java/com/felix/demoparkapi/configs/SpringDocOpenApiConfig.java
+++ demo-park-api/demo-park-api/src/main/java/com/felix/demoparkapi/configs/SpringDocOpenApiConfig.java
@@ -1,8 +1,8 @@
 package com.felix.demoparkapi.configs;
 
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,11 +16,12 @@ public class SpringDocOpenApiConfig {
                 .info(
                         new Info()
                         .title("REST API - Spring Park")
-                        .descrption("API para gestão de estacionamento")
+                        .description("API para gestão de estacionamento")
                         .version("v1")
                         .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"))
                         .contact(new Contact().name("Nathália Félix").email("nathy.felix.31@gmail.com"))
-                );
+
+        );
     }
 
 }
